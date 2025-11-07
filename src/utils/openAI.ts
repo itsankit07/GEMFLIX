6d4671395dad63bdaf88ad94b62d6d7da13f4543
Ankit Kumar Singh <91182445+itsankit07@users.noreply.github.com>
2025-11-07 12:27:39 +0530

    Update AI model from 'gemini-1.5-flash' to 'gemini-2.0-flash'

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -3,4 +3,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { OPENAI_API_KEY } from "../types/constants";
 
 const genAI = new GoogleGenerativeAI(OPENAI_API_KEY);
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
